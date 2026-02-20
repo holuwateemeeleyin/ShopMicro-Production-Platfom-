a150af711008e5c64494662a41571bef692753a9
Timileyin Ajiboye <abegundeosamuel@gmail.com>
2026-02-20 06:58:38 +0100

    Update vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 3000, host: true }
+
+  server: {
+    allowedHosts: true, // This tells Vite to accept any host (e.g., AWS URLs)
+    host: true,         // Ensures it listens on all network interfaces
+    port: 5173,
+  },
 })
