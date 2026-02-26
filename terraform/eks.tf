e67f4540a20e1849a0e79b6ed5ca1dfa89870504
Timileyin Ajiboye <abegundeosamuel@gmail.com>
2026-02-26 23:18:55 +0100

    Restructure terraform

--- terraform/eks.tf
+++
@@ -1,9 +0,0 @@
-# This is a simplified version for the project
-resource "aws_eks_cluster" "shopmicro" {
-  name     = "shopmicro-cluster"
-  role_arn = aws_iam_role.eks_cluster.arn
-
-  vpc_config {
-    subnet_ids = [aws_subnet.public.id]
-  }
-}
